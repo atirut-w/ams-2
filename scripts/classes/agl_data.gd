59202ee3a5685724a5b0ea33a18bb9765577ec2d
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-02-09 19:07:14 +0700

    Implement basic CSV loading

--- scripts/classes/agl_data.gd
+++ scripts/classes/agl_data.gd
@@ -12,4 +12,7 @@ enum DataType {
 
 
 func load_log(file: FileAccess) -> void:
-	pass
+	while file.get_position() < file.get_length():
+		var line := file.get_csv_line(PackedByteArray([0x09]).get_string_from_ascii())
+		if not (line[0].begins_with("#") || line[0].begins_with("No")):
+			breakpoint
